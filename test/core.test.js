078e3fc2bf7799cd0639ed542661a064df27109f
TomasN5 <toto_lp01@hotmail.com>
2023-04-25 19:03:10 -0300

    FEAT: test pow

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,22 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+
+describe('Pow', () => {
+    test('Deberia 5 ^ 2 = 25', () => {
+        
+        const a=5;
+        
+        const result = core.pow (a);
+        expect(result).toBe(25); 
+    })
+
+    test('No Deberia 2 ^ 2 = 10', () => {
+        
+        const a=2;
+
+        const result = core.pow (a);
+        expect(result).not.toBe(10); 
+    })
+})
