75ea1066dbdd31b1f4554d3369893bf7bd3440f8
FrancoBorsella1 <fborsella1@gmail.com>
2023-04-25 23:56:50 -0300

    TEST: divide function

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,45 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+
+describe('Divide', () => {
+    test('Deberia 6 / 2 = 3', () => {
+        //Given
+        const a = 6;
+        const b = 2;
+
+        //When
+        const result = core.div(a, b);
+
+        //Then
+        expect(result).toBe(3);
+    })
+
+    test('No deberia 10 / 2 = 4', () => {
+        //Given
+        const a = 10;
+        const b = 2;
+
+        //When
+        const result = core.div(a, b)
+
+        //Then
+        expect(result).not.toBe(4)
+    })
+
+    //Opcional
+
+    test('Deberia 10 / 0 = Error: Division por cero', () => {
+        //Given
+        const a = 10;
+        const b = 0;
+
+        //When
+        const result = core.div(a, b)
+
+        //Then
+        expect(result).toBe('Error: Division por cero');
+    })
+
+})
