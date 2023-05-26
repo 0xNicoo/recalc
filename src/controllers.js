ff0e768a3fffddc7815540069ccc79b4ccd65d99
Nicolas Morales <0nicolasmorales@gmail.com>
2023-05-25 23:45:29 -0300

    FEAT: se agrego el alta de History en el api

--- src/controllers.js
+++ src/controllers.js
@@ -39,8 +39,10 @@ router.get("/add/:a/:b", async function (req, res) {
 
     if (isNaN(a) || isNaN(b)) {
         res.status(400).send('Uno de los parámetros no es un número');
+        createHistoryEntry({a, b, operationName: "ADD", error: "no de los parámetros no es un número"})
     } else {
         const result = core.add(a, b);
+        createHistoryEntry({a, b, operationName: "ADD", result: result})
         return res.send({ result });
     }
 });
