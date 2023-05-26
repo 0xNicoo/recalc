74113d96c5637bd3ca87c5e24e744bc84a98580e
Nicolas Morales <0nicolasmorales@gmail.com>
2023-05-25 23:29:58 -0300

    TEST: Test del endpoint /add

--- test/api.test.js
+++ test/api.test.js
@@ -17,3 +17,31 @@ describe("API substract", () => {
         expect(res.body.result).toEqual(1)
     })
 })
+
+describe("API add", () => {
+    test("Deberia responder con un 200 ok y su respuesta ser menor al primer parametro", async () => {
+        const app = await api.build()
+        const a = 4
+        const b = -10
+
+        const res = await request(app).get(`/api/v1/add/${a}/${b}`)
+
+
+        expect(res.body.result).toBeLessThan(a)
+        expect(res.headers['content-type']).toBe("application/json; charset=utf-8")
+        expect(res.status).toBe(200)
+    })
+
+    test("Deberia responder con un 200 ok y su respuesta ser menor al primer parametro", async () => {
+        const app = await api.build()
+        const a = 0
+        const b = -100000
+
+        const res = await request(app).get(`/api/v1/add/${a}/${b}`)
+
+
+        expect(res.body.result).toBeLessThan(a)
+        expect(res.headers['content-type']).toBe("application/json; charset=utf-8")
+        expect(res.status).toBe(200)
+    })
+})
