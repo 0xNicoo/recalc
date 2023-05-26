5211070db13edd8f588a556ed756156234e35743
FrancoBorsella1 <fborsella1@gmail.com>
2023-05-26 00:26:42 -0300

    test: Endpoint división

--- test/api.test.js
+++ test/api.test.js
@@ -17,3 +17,14 @@ describe("API substract", () => {
         expect(res.body.result).toEqual(1)
     })
 })
+
+describe("API div", () => {
+    test("Deberia responder con un 400 y si su segundo parametro es 0 devuelva mensaje de error", async () => {
+        const app = await api.build();
+        const mensajeError="Error: Division por cero";
+
+        const res = await request(app).get("/api/v1/div/1/0")
+        expect(res.error.text).toBe(mensajeError);
+        expect(res.status).toBe(400);
+    })
+})
