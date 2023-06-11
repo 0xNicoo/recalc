be515946b1962dd0525a0ac74a5c5cdc45251782
FrancoBorsella1 <fborsella1@gmail.com>
2023-06-11 00:27:28 -0300

    chore: Se cambió el navegador a chrome para correr los tests

--- playwright.config.js
+++ playwright.config.js
@@ -25,17 +25,17 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    /*
+    
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },*/
+    },
 
-    
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
+    },*/
 /*
     {
       name: 'webkit',
