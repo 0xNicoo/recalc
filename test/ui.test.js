2bc697df7434cdef040c755740516f3fdc68a59c
FrancoBorsella1 <fborsella1@gmail.com>
2023-06-11 23:23:19 -0300

    TEST: Se hizo el test e2e para el botón de la operacion de decimal a binario

--- test/ui.test.js
+++ test/ui.test.js
@@ -180,6 +180,36 @@ test.describe('test', () => {
       expect(historyEntry.result).toEqual(25)
   });
 
+  test('Deberia poder realizar la conversión de decimal a binario',async({ page})=>{
+    await page.goto('./');
+  
+    await page.getByRole('button', { name: '4' }).click()
+    await page.getByRole('button', { name: 'bin' }).click()
+  
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/bin/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+  
+    const { result } = await response.json();
+    expect(result).toBe("100");
+  
+    await expect(page.getByTestId('display')).toHaveValue(/100/)
+  
+    const operation = await Operation.findOne({
+      where: {
+        name: "BIN"
+      }
+    });
+  
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+  
+      expect(historyEntry.firstArg).toEqual(4)
+      expect(historyEntry.result).toEqual(100)
+  });
+
 })
 
 
